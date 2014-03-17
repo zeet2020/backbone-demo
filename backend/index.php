123237d8f6e238a51d96202a2e59a2caa9a47905
zeetmail@gmail.com <zeetmail@gmail.com>
2014-03-17 20:30:07 +0530

    indentation

--- backend/index.php
+++ backend/index.php
@@ -3,124 +3,72 @@
 require 'Slim/Slim.php';
 require 'rb.php';
 \Slim\Slim::registerAutoloader();
-R::setup('mysql:host=localhost;dbname=backbone_training','root','');
+R::setup('mysql:host=localhost;dbname=backbone_training','root',''); // change database name and username and password
 session_start();
-/*$note = R::dispense('note');
-
-$note->title = "this simple note name";
-$note->body = " note body start here very importatnt data";
-$note->created = time();
-$note->guid = "easdlkasjdlkajsdkljasldkjaslkdjaskldjklasjdklasjdklas";
-
-R::store($note);*/
-
-
-
 
 $app = new \Slim\Slim(array(
-        'mode' => 'development',
-        'debug' => true
-  ));
-
-
+	//'mode' => 'development',
+    //'debug' => true
+));
 
 $app->get('/',function() use ($app){
-
-
-echo "this is back endsdf";
-
-
+	echo "speak to me!";
 });
 
 
 $app->get('/user/:id',function($id) use ($app){
-
-$_SESSION['user_guid'] = $id;
-
-echo json_encode(array('user_guid'=>$id  ));
-
- 
-
+	$_SESSION['user_guid'] = $id;
+	echo json_encode(array('user_guid'=>$id  ));
 });
 
-
-
-
 $app->get('/notes',function(){
-
-header("Content-Type: application/json");
-if(isset($_SESSION['user_guid'])){
-$guid = $_SESSION['user_guid'];
-}else{
-$guid = '';
-}
-
-$notes = R::getAll( "select * from note where guid = :guid",array('guid' => $guid));
-echo json_encode($notes);
-
+	header("Content-Type: application/json");
+	if(isset($_SESSION['user_guid'])){
+		$guid = $_SESSION['user_guid'];
+	}else{
+		$guid = '';
+	}
+	$notes = R::getAll( "select * from note where guid = :guid",array('guid' => $guid));
+	echo json_encode($notes);
 });
 
 $app->post('/notes',function() use ($app){
-$body = $app->request->getBody();
-
-
-
-
-$data = (array) json_decode($body);
-
-
- if($data){
-$note = R::dispense('note');
-$note->title = $data['title'];
-$note->body = $data['body'];
-$note->guid = $data['guid'];
-$note->created = time(); 
-$result = R::store($note);
-echo json_encode($note->export());
-
-}else{
-
-echo json_encode(array());
-
-}
-
-
-
-
-
+	$body = $app->request->getBody();
+	$data = (array) json_decode($body);
+	if($data){
+		$note = R::dispense('note');
+		$note->title = $data['title'];
+		$note->body = $data['body'];
+		$note->guid = $data['guid'];
+		$note->created = time(); 
+		$result = R::store($note);
+		echo json_encode($note->export());
+	}else{
+		echo json_encode(array());
+	}
 });
 
 
 $app->put('/notes/:id', function($id) use ($app) {
 
-$body = $app->request->getBody();
-$obj = json_decode($body);
-$note = R::load('note',$id);
-
-if($note->id){
-
-$note->title = $obj->title;
-$note->body = $obj->body;
-
-R::store($note);
-
-}
-
-
-echo json_encode($note->export());
-
-    
+	$body = $app->request->getBody();
+	$obj = json_decode($body);
+	$note = R::load('note',$id);
+	if($note->id){
+		$note->title = $obj->title;
+		$note->body = $obj->body;
+		R::store($note);
+	}
+	echo json_encode($note->export());
 });
 
 $app->delete('/notes/:id', function ($id) {
-    //Delete book identified by $id
-    $note = R::load('note',$id);
-    if($note->id){
-      R::trash($note);
-     }    
- 
-  echo json_encode(array());
-
+	//Delete book identified by $id
+	$note = R::load('note',$id);
+	if($note->id){
+		R::trash($note);
+	}    
+	echo json_encode(array());
 });
 
 $app->run();
